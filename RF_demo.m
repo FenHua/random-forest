7059ffca61786cf85134e413ce9f5451203291aa
FenHua <1556711031@qq.com>
2016-12-28 14:08:48 +0800

    random forest

---
+++ RF_demo.m
@@ -0,0 +1,16 @@
+path=['C:\Users\YAN\Desktop\visual machine learning\random forest\'];
+trainData=[path 'satimage.tra'];
+testData=[path 'satimage.txt'];
+classwt=0;
+cat0=0;
+runParam=[6 1 50 10 1 0];
+impOpt=[0 0 0];
+proCom=[0 4435 0 0 0];
+missingVal=0;
+saveForest=[1 0 0];
+runForest=[0 0];
+outParam=[1,0,0,0,0,0,0,0,0,0];
+msm=0;
+seed=4351;
+[errtr,errts,prox,trees,predictts,varimp,scale]=runRF(trainData,testData,classwt,...
+cat0,msm,runParam,impOpt,proCom,missingVal,saveForest,runForest,outParam,seed);
